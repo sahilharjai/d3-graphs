7ddd250cd777fbcfe326508e1fb9e1020867079a
Sahil Harjai <sahil.harjai99@gmail.com>
2018-01-07 23:28:49 +0530

    initial commit

---
+++ cities.js
@@ -0,0 +1,117 @@
+var cities = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"New Delhi",
+        "jobs":8
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          77.255859375,
+          28.613459424004414
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"Mumbai",
+        "jobs":32
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          72.83935546875,
+          18.906286495910905
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"Pune",
+        "jobs":22
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          73.861083984375,
+          18.521283325496277
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"Bengaluru",
+        "jobs":19
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          77.552490234375,
+          12.988500396985364
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"Chennai",
+        "jobs":35
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          80.28190612792969,
+          13.080815937441857
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"Ahmedabad",
+        "jobs":28
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          72.586669921875,
+          23.034242642076585
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"Kolkata",
+        "jobs":24
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          88.3685302734375,
+          22.573438264572395
+        ]
+      }
+    },
+    {
+      "type": "Feature",
+      "properties": {
+        "name":"Hyderabad",
+        "jobs":2
+      },
+      "geometry": {
+        "type": "Point",
+        "coordinates": [
+          78.475341796875,
+          17.371610024104744
+        ]
+      }
+    }
+  ]
+}
---
+++ dashboard/data_people.js
@@ -0,0 +1,10 @@
+var people = [
+{"id":1,"name":"Damaris" ,"gender":"Female" ,"DOB":1973-02-18 ,"MaritalStatus":false ,"CreditCardType": "visa-electron"},
+{"id":2,"name":"Damaris" ,"gender":"Female" ,"DOB":1969-04-10 ,"MaritalStatus":true ,"CreditCardType": "americanexpress"},
+{"id":3,"name":"Damaris" ,"gender":"Female" ,"DOB":1960-04-16 ,"MaritalStatus":false ,"CreditCardType": "maestro"},
+{"id":4,"name":"Damaris" ,"gender":"Female" ,"DOB":1995-01-19 ,"MaritalStatus":true ,"CreditCardType": "bankcard"},
+{"id":5,"name":"Damaris" ,"gender":"Female" ,"DOB":1992-04-16 ,"MaritalStatus":false ,"CreditCardType": "bankcard"},
+{"id":6,"name":"Damaris" ,"gender":"Female" ,"DOB":1985-01-05 ,"MaritalStatus":true ,"CreditCardType": "bankcard"},
+{"id":7,"name":"Damaris" ,"gender":"Female" ,"DOB":1956-11-01 ,"MaritalStatus":false ,"CreditCardType": "jcb"},
+{"id":8,"name":"Damaris" ,"gender":"Male" ,"DOB":1990-03-27 ,"MaritalStatus":true ,"CreditCardType": "china-unionpay"},
+];
